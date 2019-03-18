e948862eb575685f8a12884afbe3a4b14d359405
John <johnmartin3387@gmail.com>
2019-03-18 18:34:28 +0000

    add card in partner

--- ws_server/serve.js
+++ ws_server/serve.js
@@ -75,21 +75,44 @@ class HotelCardSocketServer {
 		});
 
 		client.on("delete_card_hr", function(data){
-			data = JSON.parse(data);
 			data['issue'] = "0";
-			io.sockets.emit("delete_card_hr", JSON.stringify(data));
+			data['card_id'] = data['id'];
+			if (data['user_id'] == false)
+                                io.sockets.emit("delete_card_hr", data);
+                        else {
+                                console.log(data['user_id']);
+                                var sql = "select partner_id from res_users where id='" + data['user_id'][0].toString() + "'";
+                                dbconn.sqlQuery(sql, (res)=>{
+                                        if (res.length !=0 && res[0] !== 'undefined') {
+                                                data['card_id'] = res[0]['partner_id'];
+                                                io.sockets.emit("delete_card_hr", data);
+                                        }
+                                });
+                        }
 		});
 		client.on("issue_card_hr", function(data){
-			data = JSON.parse(data);
 			data['issue'] = "1";
-			io.sockets.emit("delete_card_hr", JSON.stringify(data));
+			data['card_id'] = data['id'];
+			if (data['user_id'] == false)
+				io.sockets.emit("delete_card_hr", data);
+			else {
+				console.log(data['user_id']);
+				var sql = "select partner_id from res_users where id='" + data['user_id'][0].toString() + "'";
+				dbconn.sqlQuery(sql, (res)=>{
+                                        if (res.length !=0 && res[0] !== 'undefined') {
+						data['card_id'] = res[0]['partner_id'];
+						io.sockets.emit("delete_card_hr", data);
+					}
+				});
+			}
 		});
 		client.on("delete_card_hr_confirm", function(data){
 			data = JSON.parse(data);
-			var sql = "update res_partner set cards = '' where id = '" +
+			console.log(data);
+			var sql = "update res_partner set card_no = '' where id = '" +
 				data['partner'] + "'";
 			if (data['issue'] == "1")
-				sql = "update res_partner set cards = '"+data['cardno']+"' where id = '" +
+				sql = "update res_partner set card_no = '"+data['cardno']+"' where id = '" +
 					data['partner'] + "'";
 
 			dbconn.sqlQuery(sql, (res)=>{
@@ -111,18 +134,30 @@ class HotelCardSocketServer {
 			data = JSON.parse(data);
 
 			if (data["hotel"] == "true"){
-				var sql = "select id, card_no from res_partner card_no='" + data['cardno'] + "';";
+				var sql = "select id, card_no, customer from res_partner where card_no='" + data['cardno'] + "';";
+				// console.log(sql);
 				dbconn.sqlQuery(sql, (res)=>{
 					if (res.length !=0 && res[0] !== 'undefined') {
-						if(result_data['customer']) {
+						console.log(res)
+						if(res[0]['customer'] == true) {
 							result_data['type'] = 'customer';
 							result_data['customer_id'] = res[0]["id"];
+							io.sockets.emit("get_url", result_data);
 						}
 						else {
 							result_data['type'] = 'partner';
-							result_data['partner_id'] = res[0]["id"];
+							var sql = "select hr_employee.id as partner_id from res_partner join res_users on res_partner.id = res_users.partner_id \
+									join resource_resource on res_users.id = resource_resource.user_id \
+									join hr_employee on resource_resource.id = hr_employee.resource_id \
+									where res_partner.id='" + res[0]['id'].toString() + "';";
+                               				dbconn.sqlQuery(sql, (res)=>{
+                        			                if (res.length !=0 && res[0] !== 'undefined') {
+									result_data['partner_id'] = res[0]["partner_id"];
+                                                        		io.sockets.emit("get_url", result_data);
+                                       				}
+                                			});
 						}
-
+						// console.log(result_data);
 						io.sockets.emit("get_url", result_data);
 					}
 				});
@@ -139,7 +174,7 @@ class HotelCardSocketServer {
 
 			dbconn.sqlQuery(sql, (res)=>{
 				console.log(res);
-				console.log(sql);
+				// console.log(sql);
 				if (res.length !=0 && res[0] !== 'undefined') {
 					result_data["order_id"] = res[0]["order_id"]
 					result_data["folio_id"] = res[0]["folio_id"]
@@ -186,7 +221,7 @@ class HotelCardSocketServer {
 
 					dbconn.sqlQuery(sql, (res)=>{
 						console.log(res);
-						console.log(sql);
+						// console.log(sql);
 						if (res.length ==0 || res[0] === 'undefined') return;
 						result_data["order_id"] = res[0]["order_id"]
 						result_data["folio_id"] = res[0]["folio_id"]
